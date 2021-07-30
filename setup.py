c249a8ddfc3b9486ea61b616eef4c855762bb479
Ryu JuHeon <saidbysolo@gmail.com>
2021-07-30 13:29:50 +0900

    chore(setup): add setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools  # type: ignore
+from setuptools import setup  # type: ignore
+
+import pypixiv
+
+setup(
+    name="pypixiv",
+    author="Ryu JuHeon",
+    author_email="SaidBySolo@gmail.com",
+    url="https://github.com/SaidBySolo/pypixiv",
+    version=pypixiv.__version__,
+    long_description=open("README.md", "rt", encoding="UTF8").read(),
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),  # type: ignore
+    description="Pixiv ajax wrapper",
+    python_requires=">=3.9",
+)
