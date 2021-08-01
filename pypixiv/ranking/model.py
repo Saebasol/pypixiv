5809b833675ad73f480c954ff545f4564a9e8ec1
ombe <h3236516@gmail.com>
2021-08-02 01:34:10 +0900

    feat(ranking): create model

---
+++ pypixiv/ranking/model.py
@@ -0,0 +1,12 @@
+from pypixiv.ranking.body import ContentBody
+from typing import Any, List
+from pypixiv.abc import BaseRanking
+
+
+class RankingInfo(BaseRanking):
+    def __init__(self, response: Any):
+        super().__init__(response)
+
+    @property
+    def contents(self) -> List[ContentBody]:
+        return [ContentBody(content) for content in self.response["contents"]]
